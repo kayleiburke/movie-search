bd33a59ad3bbb678a167fd4ebe52e657aecd3a2d
Kaylei Burke <kaylei.burke@gmail.com>
2021-07-24 19:59:18 -0500

    Clean up movie model logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,34 +2,10 @@ class MoviesController < ApplicationController
   def index
     params[:movie] ||= ""
 
-    movies = Movie.retrieve_results(params[:movie])
-    unless movies && movies["Search"]
+    @movies = Movie.retrieve_results(params[:movie])
+    unless @movies.size > 0
       flash[:alert] = 'Movie not found'
-      @movies = []
-    else
-      @raw = movies
-      @movies = movies["Search"]
     end
   end
 
-  private
-
-  def request_api(url)
-    response = Excon.get(
-        url
-    )
-    return nil if response.status != 200
-    JSON.parse(response.body)
-  end
-
-  def find_movie(name)
-    if name
-      # ENV.fetch('OMDB_API_KEY')
-      request_api(
-              "http://www.omdbapi.com/?apikey=#{ENV.fetch('OMDB_API_KEY')}&s=#{URI.encode(name)}"
-      )
-    else
-      nil
-    end
-  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,20 @@
 class Movie
   include ActiveModel::Model
-  attr_accessor :data
+
+  def self.attributes
+    [:data, :poster, :plot, :title, :runtime, :rated, :year]
+  end
+
+  attr_accessor *self.attributes
+
+  def initialize(data)
+    if data
+      Movie.attributes.each do |attr|
+        instance_variable_set("@#{attr}", data[attr.capitalize.to_s])
+      end
+      @data = data
+    end
+  end
 
   def self.get_data(movie)
     @data = request_api(
@@ -10,9 +24,19 @@ class Movie
   end
 
   def self.retrieve_results(name)
-    self.request_api(
+    movies = []
+    results = self.request_api(
         "http://www.omdbapi.com/?apikey=#{ENV.fetch('OMDB_API_KEY')}&s=#{URI.encode(name)}"
     )
+
+    if results && results["Search"]
+      results["Search"].each do |result|
+        movie_data = Movie.get_data(result)
+        movies.push(Movie.new(movie_data))
+      end
+    end
+
+    movies
   end
 
   def self.request_api(url)
