3dd376a0098a653b5ebb71fdb77373aaff262a3a
Kaylei Burke <kaylei.burke@gmail.com>
2021-07-26 11:36:23 -0500

    Add more controller tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -12,6 +12,35 @@ RSpec.describe MoviesController, type: :controller do
         get :index
         expect(assigns(:movies)).to eq([])
       end
+
+      it "displays a flash message" do
+        get :index
+        expect(flash[:alert]).to eq('Movie not found')
+      end
+    end
+
+    context "with valid params" do
+      let(:params) { { movie: "food", page: 2 } }
+
+      it "renders the :index view" do
+        get :index, params: params
+        response.should render_template :index
+      end
+
+      it "returns 10 results" do
+        get :index, params: params
+        expect(assigns(:movies).size).to eq(10)
+      end
+
+      it "does not display a flash message" do
+        get :index, params: params
+        expect(flash[:alert]).to eq(nil)
+      end
+
+      it 'does paginate records' do
+        get :index, params: params
+        expect(assigns(:total_movies)).to be > 10
+      end
     end
   end
 end
