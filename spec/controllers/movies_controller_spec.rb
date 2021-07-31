e59a51205ca8836cfe992273272dcc7ac2ab25c2
Kaylei Burke <kaylei.burke@gmail.com>
2021-07-31 14:17:06 -0500

    Add tests for handling the case when no search results are returned, as well as the case where the user does not enter a search term

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe MoviesController, type: :controller do
         expect(assigns(:movies)).to eq([])
       end
 
-      it "displays a flash message" do
+      it "displays a flash message prompting the user to enter a search term" do
         get :index
-        expect(flash[:alert]).to eq('Movie not found')
+        expect(flash[:success]).to eq('Please enter a movie to search for')
       end
     end
 
@@ -40,6 +40,25 @@ RSpec.describe MoviesController, type: :controller do
       it 'does paginate records' do
         get :index, params: params
         expect(assigns(:total_movies)).to be > 10
+      end
+    end
+
+    context "with valid params that yield no results" do
+      let(:params) { { movie: "lkjlkj;klj;;k", page: 1 } }
+
+      it "renders the :index view" do
+        get :index, params: params
+        response.should render_template :index
+      end
+
+      it "returns no results" do
+        get :index, params: params
+        expect(assigns(:movies).size).to eq(0)
+      end
+
+      it "displays a flash message" do
+        get :index, params: params
+        expect(flash[:alert]).to eq("Movie not found")
       end
     end
   end
