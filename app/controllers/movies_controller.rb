78c618591ea1e99da2dbf126364c5ee3fc35fc65
Kaylei Burke <kaylei.burke@gmail.com>
2021-07-31 14:09:37 -0500

    Update alerts on the movies index page
    Display a "success" alert prompting the user to enter the name of a movie if the "movie" param is empty.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,18 +2,26 @@ class MoviesController < ApplicationController
   before_action { flash.clear }
 
   def index
+
     params[:movie] ||= ""
     params[:page] ||= 1
+    @movies = []
+    @total_movies = 0
+    alert = "Movie not found"
 
-    movie_results = Movie.retrieve_results(movie: params[:movie], page: params[:page])
-    @movies = movie_results[:movies]
+    if params[:movie] != ""
+      movie_results = Movie.retrieve_results(movie: params[:movie], page: params[:page])
+      @movies = movie_results[:movies]
 
-    unless @movies.size > 0
-      flash[:alert] = 'Movie not found'
+      @movies = Kaminari.paginate_array(@movies, total_count: movie_results[:total_movies]).page(params[:page]).per(10)
+      @total_movies = movie_results[:total_movies]
+    else
+      flash[:success] = "Please enter a movie to search for"
     end
 
-    @movies = Kaminari.paginate_array(@movies, total_count: movie_results[:total_movies]).page(params[:page]).per(10)
-    @total_movies = movie_results[:total_movies]
+    if @movies.size == 0 && params[:movie] != ""
+      flash[:alert] = "Movie not found"
+    end
 
   end
 
