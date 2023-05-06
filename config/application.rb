5f5f0ffebb4b8a271590283593d76c1b7ea319f2
Kaylei Burke <kaylei.burke@techstreet.com>
2023-05-06 15:06:04 -0500

    Try to fix the heroku compilation errors

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module OMDB
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.paths << Rails.root.join("app", "assets", "images")
-
-    config.assets.enabled = true
   end
 end
